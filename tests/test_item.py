efafa6c220076a587d2067a85d7bb07230fd490e
SimpleMeth0d <magic-roma@live.ru>
2023-11-08 20:46:19 +0300

    class + tests created

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item("Умная колонка", 4999, 40)
+Item.pay_rate = 0.9
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 199960
+
+
+def test_apply_discount():
+    item1.apply_discount()
+    assert item1.price == 4499.1
