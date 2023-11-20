6551ec37e5438109010733876d4ac3df2f05cad4
SimpleMeth0d <magic-roma@live.ru>
2023-11-20 22:56:48 +0300

    HW4. Fixed ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')

--- src/phone.py
+++ src/phone.py
@@ -13,21 +13,21 @@ class Phone(Item):
         :param number_of_sim: Количество сим-карт.
         """
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self._number_of_sim = number_of_sim
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self._number_of_sim})"
 
     def __str__(self):
         return f'{self.name}'
 
     @property
-    def sim_amount(self):
-        return self.number_of_sim
+    def number_of_sim(self):
+        return self._number_of_sim
 
-    @sim_amount.setter
-    def sim_amount(self, num):
-        """Проверяет, явояется ли новое значение целым и положительным числом"""
-        if not isinstance(num, int) and num <= 0:
-            raise ValueError('Количество физических SIM-карт должно быть целым числом или больше нуля')
-        self.number_of_sim = num
+    @number_of_sim.setter
+    def number_of_sim(self, num):
+        """Проверяет, является ли новое значение целым и положительным числом"""
+        if not isinstance(num, int) or num <= 0 or not isinstance(num, int) and num <= 0:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self._number_of_sim = num
